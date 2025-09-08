ae9c30544339484bfa235225014a03eaca028209
AkhrorKhasanov <ahror.hasanov.97@mail.ru>
2025-09-08 20:07:22 +0500

    08.09.2025 20:07

---
+++ 61A Ultra-Fast Mathematician.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s1, s2, res = "";
+    cin >> s1 >> s2;
+    for (int i = 0; i < s1.size(); i++)
+    {
+        if (s1[i] != s2[i])
+        {
+            res += '1';
+        }
+        else
+        {
+            res += '0';
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
