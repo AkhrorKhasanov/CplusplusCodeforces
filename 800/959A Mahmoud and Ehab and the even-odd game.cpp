be61ff3413b1fc9e9985db1827c313f30b834806
AkhrorKhasanov <ahror.hasanov.97@mail.ru>
2025-12-26 14:44:13 +0500

    26.12.2025 14:43

---
+++ 800/959A Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n % 2 == 1)
+    {
+        cout << "Ehab" << endl;
+        return 0;
+    }
+    cout << "Mahmoud" << endl;
+
+    return 0;
+}
