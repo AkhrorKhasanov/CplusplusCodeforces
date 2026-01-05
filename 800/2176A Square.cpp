4900fcb7c43cc84a99df337a7ec8ea3438dcafad
AkhrorKhasanov <ahror.hasanov.97@mail.ru>
2026-01-05 09:09:57 +0500

    05.01.2026 09:09

---
+++ 800/2176A Square.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        bool l = a == b && a == c && a == d;
+
+        if (l)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
