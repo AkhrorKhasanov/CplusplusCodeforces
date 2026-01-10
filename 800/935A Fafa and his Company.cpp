0c8006f6a2ad7b32c9cf5339fce4ea48c103e00b
AkhrorKhasanov <ahror.hasanov.97@mail.ru>
2026-01-10 14:25:58 +0500

    10.01.2026 14:25

---
+++ 800/935A Fafa and his Company.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, c = 0;
+    cin >> n;
+    for (int i = 1; i <= n / 2; i++)
+    {
+        if ((n - i) % i == 0)
+        {
+            c++;
+        }
+    }
+    cout << c << endl;
+    return 0;
+}
